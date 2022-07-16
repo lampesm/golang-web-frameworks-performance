c837339b38d51f9aef53e1ab2a631a3c7664565a
lampesm <smaeil0018@gmail.com>
2022-07-16 10:49:55 +0430

    change structure

---
+++ load_test/locustfile.py
@@ -0,0 +1,15 @@
+from locust import HttpUser, task, between
+
+
+class QuickstartUser(HttpUser):
+    wait_time = between(1, 2)
+
+    def on_start(self):
+        pass
+
+    @task(1)
+    def get_chi(self):
+        self.client.get(
+            f"/",
+            headers={"Content-Type": "application/json"}
+        )
