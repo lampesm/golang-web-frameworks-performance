cfa031f0b9fc188e56ad1f87449fb1b2a46089c7
lampesm <smaeil0018@gmail.com>
2022-07-19 15:16:48 +0430

    dockerize project

--- load_test/locustfile.py
+++ load_test/locustfile.py
@@ -2,13 +2,13 @@ from locust import HttpUser, task, between
 
 
 class QuickstartUser(HttpUser):
-    wait_time = between(1, 2)
+    # wait_time = between(1, 2)
 
     def on_start(self):
         pass
 
     @task(1)
-    def get_chi(self):
+    def test(self):
         self.client.get(
             f"/",
             headers={"Content-Type": "application/json"}
